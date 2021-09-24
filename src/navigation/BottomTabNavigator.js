a59f9a23e9eff90b3e77751cf20c4737eb1f2908
shawn barbel <barbels33@gmail.com>
2021-09-24 10:33:36 -0400

    restructured navigator

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import TrackDetailScreen from "./src/screens/TrackDetailScreen";
-import TrackCreateScreen from "./src/screens/TrackCreateScreen";
-import TrackListScreen from "./src/screens/TrackListScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TrackDetailScreen from "../screens/TrackDetailScreen";
+import TrackCreateScreen from "../screens/TrackCreateScreen";
+import TrackListScreen from "../screens/TrackListScreen";
 import AccountScreen from "../screens/AccountScreen";
 
 const Stack = createNativeStackNavigator();
 
 function TrackNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="List">
       <Stack.Screen name="Details" component={TrackDetailScreen} />
 
       <Stack.Screen name="List" component={TrackListScreen} />
@@ -22,9 +23,13 @@ const BottomTabNavigator = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen component={TrackNavigator} />
-      <Stack.Screen name="Create" component={TrackCreateScreen} />
+    <Tab.Navigator initialRouteName="Tracks">
+      <Tab.Screen
+        name="Tracks"
+        component={TrackNavigator}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen name="Create" component={TrackCreateScreen} />
       <Tab.Screen name="Account" component={AccountScreen} />
     </Tab.Navigator>
   );
