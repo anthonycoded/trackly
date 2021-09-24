8d8cfb21342fb9ff2ced7e0529e6d960f5427e1a
shawn barbel <barbels33@gmail.com>
2021-09-24 11:22:49 -0400

    handled inputs, styles

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,51 @@
-import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text>Sign up Screen</Text>
+    <View style={styles.container}>
+      <Text h3 style={{ marginBottom: 20 }}>
+        Sign up for Tracker
+      </Text>
+      <View style={styles.inputContainer}>
+        <Input
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+      </View>
       <Button
         title="Sign-In"
         onPress={() => navigation.navigate("Signin")}
-      ></Button>
-      <Button
-        title="Home"
-        onPress={() => navigation.navigate("BottomTab")}
       ></Button>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+  inputContainer: {
+    marginVertical: 10,
+  },
+});
 
 export default SignupScreen;
