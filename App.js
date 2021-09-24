3eb09ea755bd30f2792b02759a28e3af6054eb11
shawn barbel <barbels33@gmail.com>
2021-09-24 00:25:02 -0400

    created main navigator

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import SearchScreen from "./src/screens/SearchScreen";
+import BottomTabNavigator from "./src/navigation/BottomTabNavigator";
+
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +19,14 @@ function App() {
           headerTitle: "Food Spot Search",
         }}
       >
-        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen
+          name="BottomTab"
+          component={BottomTabNavigator}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen name="Signin" component={SigninScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
