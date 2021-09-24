bb45cd76bbebe02eb1c68624aad8be2a4c363b4c
shawn barbel <barbels33@gmail.com>
2021-09-24 13:34:29 -0400

    wrapped everything in auth provider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { Provider as AuthProvider } from "./src/context/authContext";
 import AuthNavigator from "./src/navigation/AuthNavigator";
 import BottomTabNavigator from "./src/navigation/BottomTabNavigator";
 
@@ -10,25 +11,27 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer screenOptions={{}}>
-      <Stack.Navigator
-        initialRouteName="Search"
-        screenOptions={{
-          headerTitle: "Food Spot Search",
-        }}
-      >
-        <Stack.Screen
-          name="Auth"
-          component={AuthNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="BottomTab"
-          component={BottomTabNavigator}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer screenOptions={{}}>
+        <Stack.Navigator
+          initialRouteName="Search"
+          screenOptions={{
+            headerTitle: "Food Spot Search",
+          }}
+        >
+          <Stack.Screen
+            name="Auth"
+            component={AuthNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="BottomTab"
+            component={BottomTabNavigator}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
 
