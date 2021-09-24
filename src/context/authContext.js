537dea96674e1220679c34d6da174a3e69d8db6c
shawn barbel <barbels33@gmail.com>
2021-09-24 13:48:15 -0400

    Created signin, signout, signup action scaffold

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -7,6 +7,32 @@ const authReducer = (state, action) => {
   }
 };
 
+//signup action
+const signup = (dispatch) => {
+  return ({ email, Password }) => {
+    //Make api request to sign up
+    //After sign up, Modify state that we are authenticated
+    //Sign up Failed: return error
+  };
+};
+
+//signin action
+const signin = (dispatch) => {
+  return ({ email, Password }) => {
+    //Make api request to sign in
+    //Sign in Success: Modify state that we are authenticated
+    //Sign in Failed: return error
+  };
+};
+
+//signout action
+const signout = (dispatch) => {
+  return ({ email, Password }) => {
+    //Make api request to sign out
+    //Modify state that we are authenticated
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
   {},
