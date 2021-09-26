9028bd789de4d093e771e744bef9e72ef5412c10
shawn barbel <barbels33@gmail.com>
2021-09-26 09:22:46 -0400

    removed unused code

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,11 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import AuthForm from "../components/AuthForm";
 import AuthButtons from "../components/AuthButtons";
 import { Context as AuthContext } from "../context/authContext";
 
-const SignupScreen = ({ navigation }) => {
-  const { state, signup, clearError, tryLocalSignin } = useContext(AuthContext);
-
-  useEffect(() => {
-    tryLocalSignin();
-  }, []);
+const SignupScreen = () => {
+  const { state, signup } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
