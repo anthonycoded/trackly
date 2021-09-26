d253e023dd05117c2a9d0f4cda307792c3d08b61
shawn barbel <barbels33@gmail.com>
2021-09-25 21:30:39 -0400

    imported authForm and authbuttons

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import { Context as AuthContext } from "../context/authContext";
+
+import AuthForm from "../components/AuthForm";
+import AuthButtons from "../components/AuthButtons";
 
 const SigninScreen = ({ navigation }) => {
+  const { state, signin } = useContext(AuthContext);
   return (
-    <View>
-      <Text>Sign in Screen</Text>
-      <Button
-        title="Sign-Up"
-        onPress={() => navigation.navigate("Signup")}
-      ></Button>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        buttonText="Sign-in"
+        onSubmit={signin}
+        error={state.error}
+      ></AuthForm>
+      <AuthButtons
+        text="Don't have an account?  Sign up instead."
+        routeName="Signup"
+      ></AuthButtons>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
 
 export default SigninScreen;
