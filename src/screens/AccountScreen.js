9324508c1af2b9b23c87be2f8444eb9e766e7c31
shawn barbel <barbels33@gmail.com>
2021-09-26 09:34:58 -0400

    signout button and saveAreaView

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { Button } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Context as AuthContext } from "../context/authContext";
 
 const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>AccountScreen</Text>
-    </View>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <View>
+        <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+        <Button title="Sign Out" onPress={signout}></Button>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { Context as AuthContext } from "../context/authContext";
 
 const LoadingScreen = () => {
